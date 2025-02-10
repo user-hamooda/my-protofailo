af0e53218215b63fae2a59476f29373d4dffa1ff
Mohammed Alamoudi <bl3ck.2005@gmail.com>
2025-02-10 15:44:17 +0200

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,3 +1,4 @@
+// btn nav bar
 document.getElementById('nav_mobile_btn').addEventListener('click', function () {
     const target = document.getElementById('nav_mobile');
     if (target.classList.contains('show')) {
@@ -7,9 +8,31 @@ document.getElementById('nav_mobile_btn').addEventListener('click', function () 
     }
 });
 
-document.addEventListener("DOMContentLoaded", function () {
-    setTimeout(() => {
-        document.getElementById("loading").style.display = "none";
-        document.getElementById("main-content").style.display = "block";
-    }, 2000);
-});
+// form contact
+// document.getElementById('message').addEventListener('submit', async function (e) {
+//     e.preventDefault();
+
+//     const name = document.getElementById('name').value;
+//     const email = document.getElementById('email').value;
+//     const textarea = document.getElementById('textarea').value;
+
+//     const formData = { name, email, textarea };
+
+//     try {
+//         const response = await fetch('http://localhost:3000/register', {
+//             method: 'POST',
+//             headers: { 'Content-Type': 'application/json' },
+//             body: JSON.stringify(formData)
+//         });
+
+//         if (response.ok) {
+//             alert('Registration successful!');
+//         } else {
+//             const errorMsg = await response.text();
+//             alert("Failed to register: " + errorMsg);
+//         }
+//     } catch (error) {
+//         console.error('Error:', error);
+//         alert('Error occurred while submitting the form.');
+//     }
+// });
